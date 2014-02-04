38ceb1f072a628ba3157e2f977611b8d76b45d5f
Vincent Fiduccia <vfiduccia@godaddy.com>
2014-02-04 00:32:28 -0700

    hostmap, styling

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+var USE_HOSTMAP = true;
+
 updateShowNames();
 loadHosts(connect);
 
@@ -10,7 +12,7 @@ function updateShowNames() {
   $('#hosts').toggleClass('hide-names', !show);
 };
 
-function loadHosts(cb) {
+function loadHosts(cb, url) {
   note('Loading hosts');
   ajax('/v1/hosts').then(done).fail(fail);
   
@@ -39,15 +41,17 @@ function loadHosts(cb) {
   }
 }
 
-function loadInstances(cb) {
+function loadInstances(cb, url) {
   //note('Loading instances');
-  ajax('/v1/instances').then(done).fail(fail);
+  ajax(url||'/v1/instances' + (USE_HOSTMAP ? '?include=instanceHostMaps' : '')).then(done).fail(fail);
 
   function done(res) {
     renderInstances(res.data);
 
     //note('Rendered instances');
-    if ( cb )
+    if ( res.pagination && res.pagination.next )
+      loadInstances(cb,res.pagination.next)
+    else if ( cb )
       cb();
   }
 
@@ -70,10 +74,16 @@ function renderInstances(data) {
       return;
     }
 
-    if ( !hosts[inst.requestedHostId] )
-      hosts[inst.requestedHostId] = [];
+    var hostId = 'none';
+    if ( USE_HOSTMAP && inst.instanceHostMaps && inst.instanceHostMaps.length )
+      hostId = inst.instanceHostMaps[0].hostId;
+    else if ( !USE_HOSTMAP )
+      hostId = inst.requestedHostId;
 
-    hosts[inst.requestedHostId].push( instanceTpl(inst) );
+    if ( !hosts[hostId] )
+      hosts[hostId] = [];
+
+    hosts[hostId].push( instanceTpl(inst) );
   });
 
   var keys = Object.keys(hosts);
@@ -83,16 +93,28 @@ function renderInstances(data) {
   });
 }
 
+var active = {};
+var pending = {};
 var queue = async.queue(_getInstance,4);
 
-function loadInstance(id, cb) {
-  queue.push(id, cb);
+function loadInstance(id) {
+
+  if ( pending[id] && !active[id] )
+    return;
+
+  pending[id] = 1;
+
+  queue.push(id);
 }
 
 function _getInstance(id, cb) {
-  ajax('/v1/instances/'+id).then(done).fail(fail);
+  ajax('/v1/instances/'+id+ (USE_HOSTMAP ? '?include=instanceHostMaps' : '')).then(done).fail(fail);
+  active[id] = 1;
 
   function done(res) {
+    delete pending[id];
+    delete active[id];
+
     renderInstances([res]);
 
     if ( cb )
@@ -114,11 +136,11 @@ function instanceElem(id) {
 
 function instanceColor(inst) {
   if ( inst.transitioning == 'yes' )
-    return "#0000ff";
+    return "#6666ff";
   else if ( inst.transitioning == 'error' )
-    return "#ff0000";
+    return "#ff3333";
   else
-    return "#ffffff";
+    return "#dddddd";
 }
 
 function instanceTpl(inst) {
